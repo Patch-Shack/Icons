ba29fbd76dc3e180138f487822ec117a8aff55d8
Patch-Shack <60629553+Patch-Shack@users.noreply.github.com>
2020-06-11 15:46:28 -0400

    Create Code.lua

---
+++ Code.lua
@@ -0,0 +1,22 @@
+-- download, uninstall, uninstalling, notification, comments, exit, eror, upload, gear, like
+
+local Icons = {
+	-- Downloading
+	download = "rbxassetid://5167351082",
+	uninstall = "rbxassetid://5167348115",
+	installing = "rbxassetid://5167353415",
+	
+	-- Help
+	notification = "rbxassetid://5167349354",
+	comments = "rbxassetid://5167367704",
+	
+	-- Get Out/Error
+	exit = "rbxassetid://5167354589",
+	eror = "rbxassetid://5167367172", -- spelled wrong due to error() existing
+	
+	-- Extras
+	upload = "rbxassetid://5167348613",
+	gear = "rbxassetid://5167349801",
+	like = "rbxassetid://5167354097",
+};
+return Icons
